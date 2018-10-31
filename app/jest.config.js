b7494142f62b2475f7478839185798b789434250
Robert Gulewicz <nayael@wp.pl>
2018-10-31 23:54:42 +0100

    Set up testing environment

---
+++ app/jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "roots": [
+        "<rootDir>/src"
+    ],
+    "transform": {
+        "^.+\\.tsx?$": "ts-jest"
+    },
+    "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+    "moduleFileExtensions": [
+        "ts",
+        "tsx",
+        "js",
+        "jsx",
+        "json",
+        "node"
+    ],
+    "setupTestFrameworkScriptFile": "<rootDir>/setupTests.js",
+    "moduleNameMapper": {
+        "\\.scss$": "identity-obj-proxy",
+        "^@app/(.*)$": "<rootDir>/src/app/$1",
+        "^@components$": "<rootDir>/src/app/components"
+    }
+}
