7275fd0055cb93d38a2335d25386b07fc4a257d0
Robert Gulewicz <nayael@wp.pl>
2018-11-03 22:12:06 +0100

    Subscribe to window resize event

--- app/src/app/components/ViewportScale/ViewportScale.tsx
+++ app/src/app/components/ViewportScale/ViewportScale.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { fromEvent, Subscription } from 'rxjs'
+import { map, startWith, debounceTime } from 'rxjs/operators'
 
 export namespace ViewportScale {
     export interface Props {
@@ -6,11 +8,39 @@ export namespace ViewportScale {
         ratioY: number
     }
     export interface State {
-
     }
 }
 
+interface ViewportSize {
+    width: number
+    height: number
+}
+
 export class ViewportScale extends React.PureComponent<ViewportScale.Props, ViewportScale.State> {
+
+    subscription?: Subscription;
+
+    handleResize = (vp: ViewportSize) => {
+    }
+
+    componentDidMount() {
+        this.subscription = fromEvent(window, 'resize')
+            .pipe(
+                debounceTime(250),
+                map(() => window),
+                startWith(window),
+                map(({ innerWidth, innerHeight }) => ({
+                    width: innerWidth,
+                    height: innerHeight
+                }))
+            )
+            .subscribe(this.handleResize)
+    }
+
+    componentWillUnmount() {
+        this.subscription!.unsubscribe()
+    }
+
     render() {
         return null
     }
