cc0ea5de8c9c879a9d472bbb02cb30007ef042ed
Robert Gulewicz <nayael@wp.pl>
2018-11-12 10:09:30 +0100

    Add async tests

--- app/src/app/components/RootFontSize/RootFontSize.test.tsx
+++ app/src/app/components/RootFontSize/RootFontSize.test.tsx
@@ -1,45 +1,99 @@
 import React from 'react'
 import { shallow, mount } from 'enzyme'
 
-import { RootFontSize } from './'
+import { RootFontSize, DEBOUNCE_TIME } from './RootFontSize'
+
+jest.useFakeTimers()
 
 const helpers = {
-    setViewport: (width, height) => Object.defineProperties(window, {
-        innerWidth: { value: width },
-        innerHeight: { value: height },
-    }),
-    getRootFontSize: () => document!.documentElement!.style.fontSize
+    setViewport: (width, height) => {
+        Object.defineProperties(window, {
+            innerWidth: { value: width },
+            innerHeight: { value: height },
+        })
+        window.dispatchEvent(new Event('resize'))
+    },
+    getRootFontSize: () => document!.documentElement!.style.fontSize,
+    clearRootFontSize: () => document!.documentElement!.style.fontSize = ''
 }
 
 describe('<RootFontSize />', () => {
+
+    let wrapper;
+
+    const clear = () => {
+        {
+            if (wrapper.exists()) {
+                wrapper.unmount()
+            }
+            helpers.clearRootFontSize()
+        }
+    }
+
+    afterEach(clear)
     
     it('doesn\'t render anything', () => {
-        const wrapper = shallow(<RootFontSize viewboxRatioX={1} viewboxRatioY={1} />)
+        wrapper = shallow(<RootFontSize viewboxRatioX={1} viewboxRatioY={1} />)
         expect(wrapper.type()).toEqual(null)
     })
 
     it('correctly sets root font size on mount', () => {
         helpers.setViewport(1024, 768)
-        mount(<RootFontSize viewboxRatioX={1} viewboxRatioY={1} />)
+        wrapper = mount(<RootFontSize viewboxRatioX={1} viewboxRatioY={1} />)
         expect(helpers.getRootFontSize()).toBe('7.68px')
+        clear()
         
         helpers.setViewport(768, 1024)
-        mount(<RootFontSize viewboxRatioX={1} viewboxRatioY={1} />)
+        wrapper = mount(<RootFontSize viewboxRatioX={1} viewboxRatioY={1} />)
         expect(helpers.getRootFontSize()).toBe('7.68px')
+        clear()
 
         helpers.setViewport(1024, 768)
-        mount(<RootFontSize viewboxRatioX={2} viewboxRatioY={1} />)
+        wrapper = mount(<RootFontSize viewboxRatioX={2} viewboxRatioY={1} />)
         expect(helpers.getRootFontSize()).toBe('10.24px')
+        clear()
 
         helpers.setViewport(1024, 768)
-        mount(<RootFontSize viewboxRatioX={4} viewboxRatioY={3} />)
+        wrapper = mount(<RootFontSize viewboxRatioX={4} viewboxRatioY={3} />)
         expect(helpers.getRootFontSize()).toBe('10.24px')
     })
 
-    it.skip('recalculates root font size after window resize', () => {})
+    it('recalculates root font size after window resize', () => {
+        helpers.setViewport(1024, 768)
+        wrapper = mount(<RootFontSize viewboxRatioX={1} viewboxRatioY={1} />)
+        expect(helpers.getRootFontSize()).toBe('7.68px')
 
-    it.skip('applies debounce while listening for window resize', () => {})
+        helpers.setViewport(1024, 700)
+        jest.advanceTimersByTime(DEBOUNCE_TIME)
+        expect(helpers.getRootFontSize()).toBe('7px')
 
-    it.skip('no longer reacts to window resize events after unmount', () => {})
+    })
+
+    it('applies debounce while listening for window resize', () => {
+        helpers.setViewport(1024, 768)
+        wrapper = mount(<RootFontSize viewboxRatioX={1} viewboxRatioY={1} />)
+        expect(helpers.getRootFontSize()).toBe('7.68px')
+
+        helpers.setViewport(1024, 700)
+        expect(helpers.getRootFontSize()).toBe('7.68px')
+
+        jest.advanceTimersByTime(DEBOUNCE_TIME / 2)
+        expect(helpers.getRootFontSize()).toBe('7.68px')
+        
+        jest.advanceTimersByTime(DEBOUNCE_TIME)
+        expect(helpers.getRootFontSize()).toBe('7px')
+    })
+
+    it('no longer reacts to window resize events after unmount', () => {
+        helpers.setViewport(1024, 768)
+        wrapper = mount(<RootFontSize viewboxRatioX={1} viewboxRatioY={1} />)
+        expect(helpers.getRootFontSize()).toBe('7.68px')
+
+        wrapper.unmount()
+
+        helpers.setViewport(1024, 700)
+        jest.advanceTimersByTime(DEBOUNCE_TIME * 2)
+        expect(helpers.getRootFontSize()).toBe('7.68px')
+    })
 
 })
