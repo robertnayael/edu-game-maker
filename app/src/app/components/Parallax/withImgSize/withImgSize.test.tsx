08732c16a0fdb22d4a6c1f6d989770411fb5d3b5
Robert Gulewicz <nayael@wp.pl>
2018-12-04 00:49:17 +0100

    Describe test cases

---
+++ app/src/app/components/Parallax/withImgSize/withImgSize.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { withImgSize } from './'
+
+describe('`withImgSize` HOC', () => {
+
+    it('should attempt to retrieve specified image', () => {
+    })
+
+    it('should not render anything until image has loaded', () => {
+    })
+
+    it('should render wrapped component after image has loaded', () => {
+    })
+
+    it('should pass correct imgSize after image has loaded', () => {
+    })
+
+})
