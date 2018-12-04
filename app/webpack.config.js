50ac51d1ecfaaf82cd1b80a4c55404ebd80c1683
Robert Gulewicz <nayael@wp.pl>
2018-12-04 22:08:21 +0100

    Fix alias resolution

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
         extensions: ['.js', '.ts', '.tsx'],
         mainFields: ['module', 'browser', 'main'],
         alias: {
-            app: path.resolve(__dirname, 'src/app/')
+            '@app': path.resolve(__dirname, 'src/app/')
         }
     },
     module: {
