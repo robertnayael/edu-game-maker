4fec8c24e971fabb9afac9b2676af0f8fd2dab84
Robert Gulewicz <nayael@wp.pl>
2018-12-04 01:13:34 +0100

    Implement test case

--- app/src/app/components/Parallax/withImgSize/withImgSize.test.tsx
+++ app/src/app/components/Parallax/withImgSize/withImgSize.test.tsx
@@ -2,9 +2,24 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import { withImgSize } from './'
 
+
+const mockImage = {
+    addEventListener: () => {},
+    set src(img) {}
+}
+
+window['Image'] = () => mockImage
+
 describe('`withImgSize` HOC', () => {
 
-    it('should attempt to retrieve specified image', () => {
+    it('should attempt to load specified image', () => {
+        const spy = jest.spyOn(mockImage, 'src', 'set')
+        
+        const Component = () => null
+        const ComponentWithImgSize = withImgSize(Component)
+        shallow(<ComponentWithImgSize img="☀" />)
+        
+        expect(spy).toHaveBeenCalledWith('☀')
     })
 
     it('should not render anything until image has loaded', () => {
